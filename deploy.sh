48dfe6a9045f01931a26f4a54eb697bac8c1d1dc
hendapaim <hendapaim278@gmail.com>
2022-03-25 04:35:23 +0100

    deploy heroku

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://hendapaim.github.io
-# git push -f git@github.com:hendapaim/hendapaim.github.io.git main
+git push -f git@github.com:hendapaim/hendapaim.github.io.git main
 
 # if you are deploying to https://hendapaim.github.io/<rijamente
 # git push -f git@github.com:hendapaim/<REPO>.git main:gh-pages
